eec6a1fc4cb3a68d24854b51a6fbe311e96bcff4
luvtomar2005 <luvtomar2005@gmail.com>
2026-06-10 12:30:24 +0530

    Added logging  middleware

---
+++ logging middleware/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get('/' , (req , res) => {
+    res.send("Logging middleware server running");
+});
+
+const PORT = 3000;
+
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ logging middleware/src/authservice.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const access_token = async() =>{
+    try{
+        const response = await axios.post(
+            "http://4.224.186.213/evaluation-service/auth",
+            {
+                email : process.env.EMAIL,
+                name : process.env.NAME,
+                rollNo : process.env.ROLL_NO,
+                accessCode : process.env.ACCESS_CODE,
+                clientID : process.env.CLIENT_ID,
+                clientSecret : process.env.CLIENT_SECRET,
+
+            }
+        );
+
+        return response.data.access_token;
+    }
+    catch(error){
+        console.log("There is some error in the processs");
+    }
+}
+
+
+module.exports = getAuthToken;
